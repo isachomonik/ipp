546c71fb1da64f839197c1c92b3260b3f2d152e9
Isac Homonik <dev.isachomonik@gmail.com>
2023-01-30 16:11:59 -0300

    switching project to typescript

--- middlewares/checkAdmin.js
+++
@@ -1,9 +0,0 @@
-const checkAdmin = (req, res, next) => {
-    if (req.session.loggedIn && req.session.user.admin == true) {
-      next();
-    } else {
-      res.send("You're not allowed in this section");
-    }
-  }
-
-  module.exports = checkAdmin;
--- middlewares/checkAuth.js
+++
@@ -1,9 +0,0 @@
-const checkAuth = (req, res, next) => {
-    if (req.session.loggedIn) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  }
-
-  module.exports = checkAuth;
